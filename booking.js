118de241dc600ff90e16cd3e9c7d2578e0ad945c
farazazeem-6 <faraz.tacticminds@gmail.com>
2025-08-22 18:17:39 +0500

    Dep & Des page complete

--- booking.js
+++ booking.js
@@ -1,7 +1,7 @@
 let backBtn = document.querySelector('.back-arrow i');
-
 backBtn.addEventListener('click', function () {
-    history.back()
+    localStorage.clear()
+    window.location.href = 'index.html'
 })
 
 
@@ -29,8 +29,52 @@ let setYear = document.querySelectorAll('.year');
 setDay.forEach((el, index) => {
     let thisDate = new Date(year, month, startDate + index);
     // console.log(thisDate);
-    el.textContent = days[thisDate.getDay()];
+    if (days[thisDate.getDay()] === 'Wednesday') {
+        el.textContent = spliceMonth(days[thisDate.getDay()])
+    }
+    else {
+        el.textContent = days[thisDate.getDay()];
+    }
     setDate[index].textContent = thisDate.getDate();
     setMonth[index].textContent = spliceMonth(months[thisDate.getMonth()]);
     setYear[index].textContent = ' ' + thisDate.getFullYear();
 });
+
+
+let departureInput = document.querySelector('.departure-input');
+let destinationInput = document.querySelector('.destination-input');
+
+
+let DepartureCity = localStorage.getItem('departure city')
+let DestinationCity = localStorage.getItem('destination city')
+// console.log(selectedCity);
+let departureInputValue = document.querySelector('.departure-input input')
+let destinationInputValue = document.querySelector('.destination-input input')
+departureInputValue.value = DepartureCity
+destinationInputValue.value = DestinationCity
+
+departureInput.addEventListener('click', function () {
+    window.location.href = 'departure-list.html'
+})
+destinationInput.addEventListener('click', function () {
+    window.location.href = 'destination-list.html'
+})
+
+
+let shifingArrow = document.querySelector('.shifing-arrow');
+
+
+let flag = true
+shifingArrow.addEventListener('click', function () {
+    if (flag) {
+        departureInputValue.value = DestinationCity
+        destinationInputValue.value = DepartureCity
+        flag = false
+    }
+    else {
+        departureInputValue.value = DepartureCity
+        destinationInputValue.value = DestinationCity
+        flag = true
+    }
+
+})
